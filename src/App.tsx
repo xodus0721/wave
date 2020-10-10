c5527ba6ade5eb065453d025f67d8c2c4c925252
Taeyeon <ruadl034@naver.com>
2020-10-10 22:28:01 +0900

    feat: add theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,38 @@
 import React from "react";
 import NavBar from "./components/templates/NavBar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import Main from "./components/pages/Main";
 import Properties from "./components/pages/Properties";
 
+const theme = {
+  colors: {
+    header: "#242424",
+    subHeader: "#C6C6C6",
+    lightGray: "#F7F8FA",
+    pureWhite: "#FFFFFF",
+    pureBlack: "#000000",
+    pointBlue: "#0E62FF",
+  },
+  fonts: {
+    normal: "16px",
+    small: "12px",
+    big: "72px",
+    extra: "240px",
+  },
+};
+
 function App() {
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" render={() => <Main />}></Route>
-        <Route exact path="/properties" render={() => <Properties />}></Route>
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" render={() => <Main />}></Route>
+          <Route exact path="/properties" render={() => <Properties />}></Route>
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
